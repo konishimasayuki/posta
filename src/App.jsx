d15f42cb1168d5060ed54dfd633a9621a99979ed
konishimasayuki <ninjin.konishi@gmail.com>
2026-06-03 06:33:10 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import LoginPage        from "./pages/LoginPage.jsx";
 import ProjectListPage  from "./pages/ProjectListPage.jsx";
 import GeneratePage     from "./pages/GeneratePage.jsx";
@@ -6,18 +6,29 @@ import ResultPage       from "./pages/ResultPage.jsx";
 import HistoryPage      from "./pages/HistoryPage.jsx";
 import MyPage           from "./pages/MyPage.jsx";
 import AdminPage        from "./pages/AdminPage.jsx";
+import BottomNav        from "./components/BottomNav.jsx";
+
+function Layout() {
+  const location = useLocation();
+  const hideNav = ["/login"].includes(location.pathname);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/"         element={<Navigate to="/login" replace />} />
+        <Route path="/login"    element={<LoginPage />} />
+        <Route path="/projects" element={<ProjectListPage />} />
+        <Route path="/generate" element={<GeneratePage />} />
+        <Route path="/result"   element={<ResultPage />} />
+        <Route path="/history"  element={<HistoryPage />} />
+        <Route path="/mypage"   element={<MyPage />} />
+        <Route path="/admin"    element={<AdminPage />} />
+      </Routes>
+      {!hideNav && <BottomNav />}
+    </>
+  );
+}
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/"          element={<Navigate to="/login" replace />} />
-      <Route path="/login"     element={<LoginPage />} />
-      <Route path="/projects"  element={<ProjectListPage />} />
-      <Route path="/generate"  element={<GeneratePage />} />
-      <Route path="/result"    element={<ResultPage />} />
-      <Route path="/history"   element={<HistoryPage />} />
-      <Route path="/mypage"    element={<MyPage />} />
-      <Route path="/admin"     element={<AdminPage />} />
-    </Routes>
-  );
+  return <Layout />;
 }
